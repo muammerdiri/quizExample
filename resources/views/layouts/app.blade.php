d6505db5d019d50ebda8d77e1d921d2bd68b6d5e
muammerdiri <muammerdiri2@gmail.com>
2022-02-08 11:39:34 +0300

    FontAwesome eklendi, Quizler Listelendi, Admin yönetimi eklendi. ResourceController oluşturuldu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,9 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="max-w-7xl mx-auto py-1 px-4 sm:px-5 lg:px-8">
                         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                            {{ __('Ana Sayfa') }}
+                            {{ $header}}
                         </h2>
                     </div>
                 </header>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\QuizController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,9 +11,7 @@ Route::get('/', function () {
 
 
 Route::group(['middleware'=>['auth', 'isAdmin'],'prefix'=>'admin'],function () {
-    Route::get('/deneme', function () {
-        return ('Deneme Yazısı');
-    });
+    Route::resource('quizzes', QuizController::class);
 });
 
 
